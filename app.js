96f012c637eb86c1b79401e861afb7d046e0f8b1
Prabhat Kumar Verma <56957017+prabhatk850@users.noreply.github.com>
2023-10-16 15:16:18 +0530

    add cors

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const PORT =process.env.PORT || 3000;
  require("./Database/connection")
 const Blackcoffer=require("./Database/Schema");
 const { DataModel } = require("./Database/Schema");
-
+var cors = require('cors');
 
 
 
 const app = express();
+app.use(cors());
 
 
 app.get("/",(req,res)=>{
